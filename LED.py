eb9265b633c9bb8b3ad90e2982f86f9de14fdc45
Vinicius Moreira <vinicius741@gmail.com>
2018-06-07 11:53:45 -0300

    errada

--- LED.py
+++ LED.py
@@ -2,36 +2,36 @@ import RPi.GPIO as GPIO
 import time
 
 class LED:
-    __pinled = 14
-    __durationtime = 1
+    __pinled__ = 14
+    __durationtime__ = 1
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(self.__pinled,GPIO.OUT)
+        GPIO.setup(self.__pinled__,GPIO.OUT)
 
     def setpinled(self, pin):
-        self.__pinled = pin
-        GPIO.setup(self.__pinled,GPIO.OUT)
+        self.__pinled__ = pin
+        GPIO.setup(self.__pinled__,GPIO.OUT)
 
     def setdurationtime(self, time):
-        self.__durationtime = time
+        self.__durationtime__ = time
     
     def sendsignalled(self, typeofsignal):
         if typeofsignal == "on":
-            GPIO.output(self.__pinled,GPIO.HIGH) # Liga o led
-            time.sleep(self.__durationtime)
-            GPIO.output(self.__pinled,GPIO.LOW)
+            GPIO.output(self.__pinled__,GPIO.HIGH) # Liga o led
+            time.sleep(self.__durationtime__)
+            GPIO.output(self.__pinled__,GPIO.LOW)
 
         elif typeofsignal == "off":
-            time.sleep(self.__durationtime)
+            time.sleep(self.__durationtime__)
 
         elif typeofsignal == "blink":
-            blinktime = __durationtime/5
+            blinktime = self.__durationtime__/5
 
             for i in range(0, 5):
-                GPIO.output(self.__pinled,GPIO.HIGH) # Liga o led
-                time.sleep(self.blinktime)
-                GPIO.output(self.__pinled,GPIO.LOW)
+                GPIO.output(self.__pinled__,GPIO.HIGH) # Liga o led
+                time.sleep(blinktime)
+                GPIO.output(self.__pinled__,GPIO.LOW)
 
 
 l = LED()
