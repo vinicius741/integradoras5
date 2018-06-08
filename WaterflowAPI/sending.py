ce51bad7ba295aac6d46cfcd31e644f3287408d7
Vinicius Moreira <vinicius741@gmail.com>
2018-06-08 13:37:01 -0300

    bug fixs

--- WaterflowAPI/sending.py
+++ WaterflowAPI/sending.py
@@ -1,12 +1,12 @@
 import socket
 
 UDP_IP = "192.168.1.6"
-UDP_PORT = 555
+UDP_PORT = "555"
 MESSAGE = "R"
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+print ("message:", MESSAGE)
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
--- udpsender.py
+++ udpsender.py
@@ -4,10 +4,9 @@ UDP_IP = "192.168.1.4"
 UDP_PORT = 5005
 MESSAGE = "Hello, World!"
 
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+print ("message:", MESSAGE)
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock = socket.socket(socket.AF_INET,  socket.SOCK_DGRAM) # UDP
+sock.sendto(MESSAGE.encode(), (UDP_IP , UDP_PORT))
