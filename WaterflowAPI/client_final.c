4940a74dd3ff9a120ccb006482b385069ee8880b
João Regis <joao.reegiis@gmail.com>
2018-06-09 18:37:03 -0300

    Update client_final.c

--- WaterflowAPI/client_final.c
+++ WaterflowAPI/client_final.c
@@ -148,32 +148,8 @@ void do_listen() {
         Serial.print("Received data: ");
         Serial.println(data);
 
-        readedValue = toInt(data);
+        readedValue = atoi(data);
 
         udp.flush();
     }
-}
 
-int toInt(char * str)
-{
-    int result;
-    int puiss;
-
-    result = 0;
-    puiss = 1;
-
-    while (('-' == (*str)) || ((*str) == '+'))
-    {
-        if (*str == '-')
-        puiss = puiss * -1;
-        str++;
-    }
-
-    while ((*str >= '0') && (*str <= '9'))
-    {
-      result = (result * 10) + ((*str) - '0');
-      str++;
-    }
-
-    return (result * puiss);
-}
